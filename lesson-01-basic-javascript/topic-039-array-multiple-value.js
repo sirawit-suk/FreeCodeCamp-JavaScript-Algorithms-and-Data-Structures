f6d6699fb923570628bcd5e6791e62fddab57768
Sirawit <popeye-zz@hotmail.com>
2022-11-06 02:38:56 +0700

    add topic 025-050

---
+++ lesson-01-basic-javascript/topic-039-array-multiple-value.js
@@ -0,0 +1,2 @@
+// Only change code below this line
+var myArray = ["s", 2];
---
+++ lesson-01-basic-javascript/topic-045-array-shift.js
@@ -0,0 +1,10 @@
+// Setup
+const myArray = [
+  ["John", 23],
+  ["dog", 3],
+];
+
+// Only change code below this line
+console.log(myArray)
+const removedFromMyArray = myArray.shift(); // remove the most left out
+console.log("remove:",removedFromMyArray,"myArray:", myArray)
