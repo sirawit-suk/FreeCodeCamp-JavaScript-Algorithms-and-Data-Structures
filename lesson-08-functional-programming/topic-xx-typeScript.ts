a7f8d4769bd7a5e7f959d50f945093529faf83b1
Sirawit <popeye-zz@hotmail.com>
2022-11-12 16:51:01 +0700

    rename variable in topic xx

--- lesson-08-functional-programming/topic-xx-typeScript.ts
+++ lesson-08-functional-programming/topic-xx-typeScript.ts
@@ -10,10 +10,10 @@ console.log(passArrayFunc(list, 10));
 console.log(list)
 
 
-const a = "a"
-const b = "b"
+const firstChar = "a"
+const secondChar = "b"
 
-console.log(a.charCodeAt(0) - b.charCodeAt(0))
+console.log(firstChar.charCodeAt(0) - secondChar.charCodeAt(0))
 console.log(list.sort((a, b) => b - a));
 
 
